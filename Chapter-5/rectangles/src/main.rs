c5812b55051fe1b21eb6e651e06b23b6e75e990f
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 09:20:56 -0700

    associated Functions

--- Chapter-5/rectangles/src/main.rs
+++ Chapter-5/rectangles/src/main.rs
@@ -9,14 +9,24 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50};
+    let rect2 = Rectangle { width: 10, height: 40};
+    let rect3 = Rectangle { width: 60, height: 45};
+    let sq = Rectangle::square(3);
 
-    println!(
-        "The area of the rectangle is {} square pixels.", 
-        rect1.area()
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    );
+    println!("Can sq hold rect1? {}", sq.can_hold(&rect1));
 }
