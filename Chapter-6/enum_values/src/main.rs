63e17fc62121a22bba39e91766876bc761a984b9
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 09:35:06 -0700

    basic enum example

---
+++ Chapter-6/enum_values/src/main.rs
@@ -0,0 +1,19 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    V4(String),
+    V6(String),
+}
+
+
+
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+
+let loopback = IpAddr::V6(String::from("::1"));
+}
---
+++ Chapter-6/enum_values/target/debug/deps/enum_values-f0e137ad01e104cd.d
@@ -0,0 +1,5 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-6/enum_values/target/debug/deps/enum_values-f0e137ad01e104cd: src/main.rs
+
+/Users/merrill/workspace/rust_tutorial/Chapter-6/enum_values/target/debug/deps/enum_values-f0e137ad01e104cd.d: src/main.rs
+
+src/main.rs:
