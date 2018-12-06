944481490985f602f20a4d59a83043cdc37fa406
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 11:23:12 -0700

    feature-320/fix

--- run.sh
+++ run.sh
@@ -3,11 +3,10 @@
 for i in {1..100}
 do
     echo "looping ... number $i"
-    filename=$((RANDOM % 60 + 30))
+    filename=$((RANDOM % 1234 + 30))
     echo "filename: $filename"
     for ((j=1; j <=$filename; j++))
     do
-        echo "internal loop $j"
         echo "$(($RANDOM % 60 + $RANDOM % 25505))" >> $filename.txt
     done
     git add .
