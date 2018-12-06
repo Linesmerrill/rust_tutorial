06cc2598c7c9fc1fb5f8a31b54a81459d54eb77f
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 09:14:35 -0700

    utilizing methods rather than functions

--- Chapter-5/rectangles/src/main.rs
+++ Chapter-5/rectangles/src/main.rs
@@ -5,19 +5,18 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50};
 
     println!(
         "The area of the rectangle is {} square pixels.", 
-        area(&rect1)
+        rect1.area()
 
     );
-
-    println!("rect1 is {:#?}", rect1); //little debug statement to view the data structure
-
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
