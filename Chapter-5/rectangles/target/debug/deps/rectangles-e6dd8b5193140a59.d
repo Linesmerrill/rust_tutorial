85ac81be1551531534a5291f56447f5e72e97408
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 09:01:25 -0700

    add chapter folders for 4 and 5. rectangle with tuples

---
+++ Chapter-5/rectangles/target/debug/deps/rectangles-e6dd8b5193140a59.d
@@ -0,0 +1,5 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-5/rectangles/target/debug/deps/rectangles-e6dd8b5193140a59: src/main.rs
+
+/Users/merrill/workspace/rust_tutorial/Chapter-5/rectangles/target/debug/deps/rectangles-e6dd8b5193140a59.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter-5/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-5/rectangles/target/debug/rectangles.dSYM: /Users/merrill/workspace/rust_tutorial/Chapter-5/rectangles/src/main.rs
