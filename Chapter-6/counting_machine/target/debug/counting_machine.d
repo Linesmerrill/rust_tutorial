a5b43a8781c93c4abb642d87e1ef8e965d51f484
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 09:42:26 -0700

    adding basic coins with states

---
+++ Chapter-6/counting_machine/target/debug/counting_machine.d
@@ -0,0 +1,1 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-6/counting_machine/target/debug/counting_machine.dSYM: /Users/merrill/workspace/rust_tutorial/Chapter-6/counting_machine/src/main.rs
---
+++ Chapter-6/counting_machine/target/debug/deps/counting_machine-13a2129ca2317b5b.d
@@ -0,0 +1,5 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-6/counting_machine/target/debug/deps/counting_machine-13a2129ca2317b5b: src/main.rs
+
+/Users/merrill/workspace/rust_tutorial/Chapter-6/counting_machine/target/debug/deps/counting_machine-13a2129ca2317b5b.d: src/main.rs
+
+src/main.rs:
