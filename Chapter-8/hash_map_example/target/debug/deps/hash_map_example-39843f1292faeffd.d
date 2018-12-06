691ab7a70067c245a0d94f0d9a66588378a056c8
Merrill Lines <merrill@Merrills-iMac.local>
2018-12-06 10:04:23 -0700

    creating a new hash map

---
+++ Chapter-8/hash_map_example/target/debug/deps/hash_map_example-39843f1292faeffd.d
@@ -0,0 +1,5 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-8/hash_map_example/target/debug/deps/hash_map_example-39843f1292faeffd: src/main.rs
+
+/Users/merrill/workspace/rust_tutorial/Chapter-8/hash_map_example/target/debug/deps/hash_map_example-39843f1292faeffd.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter-8/hash_map_example/target/debug/hash_map_example.d
@@ -0,0 +1,1 @@
+/Users/merrill/workspace/rust_tutorial/Chapter-8/hash_map_example/target/debug/hash_map_example.dSYM: /Users/merrill/workspace/rust_tutorial/Chapter-8/hash_map_example/src/main.rs
